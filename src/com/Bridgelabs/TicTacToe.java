858d8744b4ca8affb237acac00c7b2d1f4c447b9
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-10-02 23:34:57 +0530

    Add: Ability to get input from both user and computer

--- src/com/Bridgelabs/TicTacToe.java
+++ src/com/Bridgelabs/TicTacToe.java
@@ -1,13 +1,22 @@
 package com.Bridgelabs;
 
+import java.util.*;
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
-	//creating char array of size 10
+	
+	/**
+	 * creating char array of size 10
+	 * used scanner to get user input
+	 * created two instance variables for player and computer
+	 * used random to generate computer choice 
+	 */
 	char[] gameBoard = new char[10];
 	Scanner scanner = new Scanner(System.in);
 	char playerLetter;
 	char computerLetter;
+	Random random = new Random();
 	
 	/**
 	 * 
@@ -35,17 +44,17 @@ public class TicTacToe {
 			if (choice == 1) {
 				playerLetter = 'X';
 				computerLetter = 'O';
-				System.out.println("Player Letter: " + playerLetter);
+				System.out.println("\n\nPlayer Letter: " + playerLetter);
 				System.out.println("Computer Letter: " + computerLetter);
 				condition =1;
 			}else if(choice == 2) {
 				playerLetter = 'X';
 				computerLetter = 'O';
-				System.out.println("Player Letter: " + playerLetter);
+				System.out.println("\n\nPlayer Letter: " + playerLetter);
 				System.out.println("Computer Letter: " + computerLetter);
 				condition = 2;
 			}else {
-				System.out.println("*****Enter 1 OR 2 *****");
+				System.out.println("\n*****Enter 1 OR 2 *****");
 			}
 		}
 	}
@@ -61,15 +70,37 @@ public class TicTacToe {
         System.out.println(gameBoard[4] + "   | "+ gameBoard[5] + "| " + gameBoard[6]);
         System.out.println("----+--+----");
         System.out.println( gameBoard[7] + "   | "+ gameBoard[8] +"| " + gameBoard[9]);
-        
-		
+        		
 	}
+	/**
+	 * 
+	 * creating method so user can input its desired position
+	 * enter value from 1-9
+	 * Computer input is also made by help of random
+	 */
+	void gameBoardInput() {
+		System.out.println();
+		System.out.println("\n\n***** Player Turn *****");
+		System.out.println("Enter your choice position between(1-9)");
+		int playerPosition = scanner.nextInt();
+		gameBoard[playerPosition] = playerLetter;
+		displayingBoard();
+	}		
+	void computerInput() {
+		System.out.println("***** Computer Turn *****");
+		int computerPosition = random.nextInt(9) + 1;
+		gameBoard[computerPosition] = computerLetter;
+		displayingBoard();
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("********************************************************\n" +"    WELCOME TO THE TIC TAC TOE GAME"+ "\n********************************************************");	
 		TicTacToe ticTacToe = new TicTacToe();
 		ticTacToe.chooseOptionForPlay();
 		ticTacToe.boardCreation();
 		ticTacToe.displayingBoard();
+		ticTacToe.gameBoardInput();
+		ticTacToe.computerInput();
 	}
 
 }
