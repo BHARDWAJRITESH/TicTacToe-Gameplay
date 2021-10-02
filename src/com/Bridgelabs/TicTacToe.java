f9a73670a2b70b8f6173abb304686bb74baeabf8
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-10-02 20:47:48 +0530

    Add: Welcome message

---
+++ src/com/Bridgelabs/TicTacToe.java
@@ -0,0 +1,12 @@
+package com.Bridgelabs;
+
+public class TicTacToe {
+
+	public static void main(String[] args) {
+		System.out.println("********************************************************\n" +"    Welcome to the TIC TAC TOE game"+ "\n********************************************************");	
+
+			
+
+	}
+
+}
